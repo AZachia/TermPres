18f90328b604b6922afcdc930a309f5003217fbd
AZachia <a.zachia@hotmail.com>
2024-04-01 18:53:15 +0200

    add demo

---
+++ demo.py
@@ -0,0 +1,16 @@
+from TermPres import TermPresentation, Slide
+
+
+pres = TermPresentation()
+
+
+slide = Slide("Title")
+slide.add_text("This is a slide")
+slide.add_enum(["you can add", "a list", "of items"])
+slide.add_text("And even images:")
+slide.add_image("mario.png", h=30, w=50, align="left", x=5)
+
+
+pres.add_slide(slide)
+
+pres.show()
